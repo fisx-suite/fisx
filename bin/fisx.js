0ddc2da6f094eaba89f85066b6ae4297f57bf598
wuhy <sparklewhy@gmail.com>
2015-12-28 18:06:07 +0800

    fix windows7 modulename missing problem

--- bin/fisx.js
+++ bin/fisx.js
@@ -8,10 +8,11 @@
 var Liftoff = require('liftoff');
 var argv = require('minimist')(process.argv.slice(2));
 var path = require('path');
+var solutionName = 'fisx';
 var cli = new Liftoff({
-    name: 'fisx',
-    processTitle: 'fisx',
-    moduleName: 'fisx',
+    name: solutionName,
+    processTitle: solutionName,
+    moduleName: solutionName,
     configName: 'fis-conf',
 
     // only js supported!
@@ -31,6 +32,10 @@ cli.launch({
     else {
         fis = require(env.modulePath);
     }
+
+    // windows 7 下 模块 name 名称可能不存在，这里强制赋值
+    env.modulePackage || (env.modulePackage = {});
+    env.modulePackage.name = solutionName;
 
     fis.set('system.localNPMFolder', path.join(env.cwd, 'node_modules/fis3'));
     fis.set('system.globalNPMFolder', path.dirname(__dirname));
